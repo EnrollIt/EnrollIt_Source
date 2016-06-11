7484214de9b8deed11893d0e14cbab6cad601235
NKM <neerajmohinani@gmail.com>
2016-06-12 03:10:46 +0530

    basic folder structure corrected.

---
+++ EnrollIt/src/main/java/com/enrollIt/models/Address.java
@@ -0,0 +1,40 @@
+package com.enrollIt.models;
+
+public class Address {
+	private String pincode;
+	private String area;
+	private String city;
+	private String state;
+	private String landmark;
+	public String getPincode() {
+		return pincode;
+	}
+	public void setPincode(String pincode) {
+		this.pincode = pincode;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getLandmark() {
+		return landmark;
+	}
+	public void setLandmark(String landmark) {
+		this.landmark = landmark;
+	}
+
+}
